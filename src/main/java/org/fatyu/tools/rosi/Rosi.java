7971e02302bc4e3df2ec4ee1090b9e77cacf0dd3
zhangyu <zhangy@miqtech.com>
2016-07-19 13:58:20 +0800

    ...

---
+++ src/main/java/org/fatyu/tools/rosi/Rosi.java
@@ -0,0 +1,7 @@
+package org.fatyu.tools.rosi;
+
+public class Rosi {
+	public static String mode_001 = "http://mm.dulei.si/%SEASON%/ROSI_%SEASON%_%IMG_NUM%.JPG";
+	public static String mode_002 = "http://mm.dulei.si/NO.%SEASON%/rosimm-%SEASON%-%IMG_NUM%.jpg";
+	public static String mode_003 = "http://mm.dulei.si/%SEASON%/ROSI_%SEASON%_%IMG_NUM%.jpg";
+}
